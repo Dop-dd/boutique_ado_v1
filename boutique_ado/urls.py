cce497e43a9fd0691bcb1c58e5b2bb5d77127c3d
Dop-dd <dopdd@yahoo.com>
2020-11-03 18:36:10 +0000

    added home page media

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# And then use the static function to add the MEDIA_URL to our list of URLs.
